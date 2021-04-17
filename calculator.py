2336eca749dfd42291bc39f52520ed5a65535dc7
Forrest1711 <forrest.90h@gmail.com>
2021-04-16 21:14:21 -0700

    finished calculator with command line arguments

--- calculator.py
+++ calculator.py
@@ -1,10 +1,26 @@
-def add(n,m)
-    return n+m
-def subtract(n,m)
+import sys
+
+
+def add(n, m):
+    return (n + m)
+
+
+def subtract(n, m):
     return n-m
-def divide(n,m)
+
+
+def divide(n, m):
     return n/m
-def multiply(n,m)
+
+
+def multiply(n, m):
     return n*m
 
-# do stuff
+
+N = int(sys.argv[1])
+M = int(sys.argv[2])
+
+print(N, " + ", M, " = ", add(N, M))
+print(N, " - ", M, " = ", subtract(N, M))
+print(N, " / ", M, " = ", divide(N, M))
+print(N, " * ", M, " = ", multiply(N, M))
